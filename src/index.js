a19745a4c99a9b2d91dc9e2b9eb3e0184e4c765b
Nelson Mbau <mbau.ndungu@gmail.com>
2024-04-11 11:12:14 +0300

    feat: add event listeners for all buttons

--- src/index.js
+++ src/index.js
@@ -25,8 +25,142 @@ const likeQuote = async (id) => {
   return data;
 };
 
+const unlikeQuote = async (id) => {
+  const response = await fetch(`${dbUrl}/quotes/${id}`, {
+    method: "DELETE",
+  });
+  const data = await response.json();
+  return data;
+};
+
 const getLikedQuotes = async () => {
   const response = await fetch(`${dbUrl}/quotes`);
   const data = await response.json();
   return data;
 };
+
+const renderQuote = async (quote) => {
+  console.log({ quote });
+  const list = document.querySelector("#quote-box .list-group");
+  while (list.firstChild) {
+    list.removeChild(list.firstChild);
+  }
+
+  const listElement = document.createElement("li");
+  listElement.classList.add("list-group-item");
+
+  const card = document.createElement("div");
+  card.classList.add("card");
+  card.id = `card-${quote.id}`;
+
+  const cardBody = document.createElement("div");
+  cardBody.classList.add("card-body");
+  cardBody.textContent = quote.content;
+
+  const cardFooter = document.createElement("div");
+  cardFooter.classList.add("card-footer");
+  cardFooter.classList.add("d-flex");
+  cardFooter.classList.add("justify-content-end");
+
+  const likeBtn = document.createElement("button");
+  likeBtn.classList.add("btn");
+  likeBtn.classList.add("btn-outline-primary");
+  likeBtn.textContent = "Like Quote";
+
+  likeBtn.addEventListener("click", async () => {
+    const data = await likeQuote(quote._id);
+    if (data.liked) {
+      const [nextQuote] = await getQuotes();
+      renderQuote(nextQuote);
+    } else {
+      const [nextQuote] = await getQuotes();
+      renderQuote(nextQuote);
+    }
+  });
+
+  cardFooter.appendChild(likeBtn);
+  card.appendChild(cardBody);
+  card.appendChild(cardFooter);
+
+  listElement.appendChild(card);
+  list.appendChild(listElement);
+};
+
+const renderLikedQuote = async (quote) => {
+  console.log({ quote });
+  const list = document.querySelector("#liked-quote-box .list-group");
+
+  const listElement = document.createElement("li");
+  listElement.classList.add("list-group-item");
+
+  const card = document.createElement("div");
+  card.classList.add("card");
+  card.id = `card-${quote.id}`;
+
+  const cardBody = document.createElement("div");
+  cardBody.classList.add("card-body");
+  cardBody.textContent = quote.content;
+
+  const cardFooter = document.createElement("div");
+  cardFooter.classList.add("card-footer");
+  cardFooter.classList.add("d-flex");
+  cardFooter.classList.add("justify-content-end");
+
+  const likeBtn = document.createElement("button");
+  likeBtn.classList.add("btn");
+  likeBtn.classList.add("btn-outline-danger");
+  likeBtn.textContent = "Unlike Quote";
+
+  likeBtn.addEventListener("click", async () => {
+    const data = await unlikeQuote(quote._id);
+    if (data.liked) {
+      const [nextQuote] = await getQuotes();
+      renderQuote(nextQuote);
+    } else {
+      const [nextQuote] = await getQuotes();
+      renderQuote(nextQuote);
+    }
+  });
+
+  cardFooter.appendChild(likeBtn);
+  card.appendChild(cardBody);
+  card.appendChild(cardFooter);
+
+  listElement.appendChild(card);
+  list.appendChild(listElement);
+};
+
+document.addEventListener("DOMContentLoaded", async () => {
+  const nextQuoteBtn = document.getElementById("next-quote");
+  const likedQuotesBtn = document.getElementById("liked-quotes");
+  const quoteBox = document.getElementById("quote-box");
+  const likedQuotesBox = document.getElementById("liked-quote-box");
+  const homeBtn = document.getElementById("home");
+
+  const [quote] = await getQuotes();
+  await renderQuote(quote);
+
+  homeBtn.addEventListener("click", async () => {
+    quoteBox.classList.remove("hidden");
+    likedQuotesBox.classList.add("hidden");
+
+    const [quote] = await getQuotes();
+    await renderQuote(quote);
+  });
+
+  nextQuoteBtn.addEventListener("click", async () => {
+    const [quote] = await getQuotes();
+    await renderQuote(quote);
+  });
+
+  likedQuotesBtn.addEventListener("click", async () => {
+    quoteBox.classList.add("hidden");
+    likedQuotesBox.classList.remove("hidden");
+
+    const quotes = await getLikedQuotes();
+    quotes.map(async ({ quoteId }) => {
+      const quote = await getSingleQuote(quoteId);
+      renderLikedQuote(quote);
+    });
+  });
+});
